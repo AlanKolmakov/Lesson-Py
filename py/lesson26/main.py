74aa880b208aeadca0c23ec99bd9fdc74b8e7cee
AlanKolmakov <kolm.a.s.1983@gmail.com>
2022-02-25 22:39:49 +0800

    Структуры данных.

--- py/lesson26/main.py
+++ py/lesson26/main.py
@@ -116,6 +116,46 @@ class LinkedList:
             current = current.get_next()
         return False
 
+    def remove(self, item):
+        if self.search(item):
+            index = self.index(item)
+            self.pop(index)
+
+    def index(self, item):
+        pos = 0
+        current = self.head
+        while current is not None:
+            if current.get_data() == item:
+                return pos
+            pos += 1
+            current = current.get_next()
+        return None
+
+    def pop(self, position=None):
+        current = self.head
+        if position is None:
+            current.get_data()
+            self.head = current.get_next()
+        elif position > self.size() - 1:
+            raise IndexError("Индекс находится за пределами списка")
+        else:
+            pos = 0
+            previous = None
+            while pos < position:
+                previous = current
+                current = current.get_next()
+                pos += 1
+                current.get_data()
+            previous.set_next(current.get_next())
+
+    def reverse(self):
+        p = self.head
+        self.head = None
+        while p is not None:
+            p0, p = p, p.get_next()
+            p0.set_next(self.head)
+            self.head = p0
+
 
 # temp = Node(93)
 # print(temp.get_data())
@@ -133,4 +173,158 @@ temp.list_print()
 print(temp.size())
 temp.insert(3, 17)
 temp.list_print()
-print(temp.search(18))
+print(temp.search(17))
+temp.pop(2)
+temp.list_print()
+print(temp.index(77))
+temp.list_print()
+temp.remove(17)
+temp.list_print()
+
+# двусвязный список
+
+# class Node:
+#     def __init__(self, elem, nxt=None, prev=None):
+#         self.data = elem
+#         self.prev = prev
+#         self.next = nxt
+#
+#
+# class DoubleLinkedList:
+#     def __init__(self, head=None, tail=None):
+#         self.head = head
+#         self.tail = tail
+#
+#     def add(self, elem):
+#         if self.head is None:
+#             item = Node(elem)
+#             self.head = item
+#             self.tail = self.head
+#         else:
+#             item = Node(elem, self.head)
+#             self.head.prev = item
+#             self.head = self.head.prev
+#
+#     def print(self):
+#         val = self.head
+#         print("Список ссылок: ")
+#         while val:
+#             print(val.data)
+#             val = val.next
+#         print()
+#
+#     def is_empty(self):
+#         return self.head is None and self.tail is None
+#
+#     def append(self, elem):
+#         if self.tail is None:
+#             self.add(elem)
+#         else:
+#             item = Node(elem, None, self.tail)
+#             self.tail.next = item
+#             self.tail = item
+#
+#     def pop(self):
+#         if self.head == self.tail:
+#             self.head = self.tail = None
+#             return
+#         self.tail = self.tail.prev
+#         self.tail.next = None
+#
+#     def shift(self):
+#         if self.head == self.tail:
+#             self.head = self.tail = None
+#             return
+#         self.head = self.head.next
+#         self.head.prev = None
+#
+#
+# links = [
+#     'http://site.ru',
+#     'http://site.ru/news',
+#     'http://site.ru/contacts',
+#     'http://site.ru/about'
+# ]
+#
+# link = DoubleLinkedList()
+# for name in links:
+#     link.add(name)
+#
+# while True:
+#     if not link.is_empty():
+#         link.print()
+#     else:
+#         print("Список ссылок пуст")
+#     print('Меню')
+#     print("1 - Добавить элемент в начало списка")
+#     print("2 - Добавить элемент в конец списка")
+#     print("3 - Удалить элемент их конца списка")
+#     print("4 - Удалить элемент из начала списка")
+#     print("0 - Выход")
+#     operation = input('-> ')
+#     if operation == '1':
+#         a = input("Новая ссылка: ")
+#         link.add(a)
+#     elif operation == '2':
+#         a = input("Новая ссылка: ")
+#         link.append(a)
+#     elif operation == '3':
+#         link.pop()
+#     elif operation == '4':
+#         link.shift()
+#     elif operation == '0':
+#         print("Всего доброго!!!")
+#         break
+
+# Стек - "последний пришел - первым ушел" (LIFO).
+# Очередь - "первым пришел - первым ушел" (FIFO).
+
+# class Stack:
+#     def __init__(self):
+#         self.stack = []
+#
+#     def __str__(self):
+#         return f"{self.stack}"
+#
+#     def push(self, item):
+#         self.stack.append(item)
+#
+#     def size(self):
+#         return len(self.stack)
+#
+#     def pop(self):
+#         if len(self.stack) == 0:
+#             return None
+#         return self.stack.pop()
+#
+#
+# a = Stack()
+# a.push(1)
+# a.push(2)
+# a.push(3)
+# print(a)
+# print(a.size())
+# a.pop()
+# a.pop()
+# print(a)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def __str__(self):
+        return f"{self.stack}"
+
+    def is_empty(self):
+        return self.stack == []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
